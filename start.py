55f2ec8bb5035a7376c5029b2b895824377ae28f
=Wajuma <=meltusjuma5@gmail.com>
2021-10-17 18:20:41 +0300

    uppload app

---
+++ start.py
@@ -0,0 +1,12 @@
+from kivymd.app import MDApp
+from kivy.core.window import Window
+
+Window.size =(320, 600)
+
+class Screenbuild(MDApp):
+	def build(self):
+		return
+
+
+if __name__ =='__main__':
+	Screenbuild().run()
